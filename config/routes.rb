4077e298c0a24d3938336985dc9c7e5c85c6b416
tayyabmaqsood48 <tayyab.maqsood@devsinc.com>
2021-10-02 01:40:54 +0500

    albums controller created

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,9 @@
 Rails.application.routes.draw do
+  get 'albums/index'
+  get 'albums/new'
+  get 'albums/show'
+  get 'album/index'
+  get 'album/new'
+  get 'album/show'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end

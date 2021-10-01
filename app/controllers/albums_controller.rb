a2c2d256caded18a71cc836d493db422e1ffb5de
tayyabmaqsood48 <tayyab.maqsood@devsinc.com>
2021-10-02 02:17:51 +0500

    CRD operation

--- app/controllers/albums_controller.rb
+++ app/controllers/albums_controller.rb
@@ -1,10 +1,34 @@
 class AlbumsController < ApplicationController
   def index
+    @albums = Album.all
   end
 
   def new
+    @album = Album.new
+  end
+
+  def create
+    @album = Album.new(album_params)
+    if @album.save
+      redirect_to @album
+    else
+      render 'new'
+    end
   end
 
   def show
+    @album = Album.find(params[:id])
+  end
+
+  def destroy
+    @album = Album.find_by(params[:id])
+    @album.destroy
+    redirect_to albums_path
+  end
+
+  private
+
+  def album_params
+    params.require(:album).permit(:name, :description,:owner)
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,4 @@
 Rails.application.routes.draw do
-  get 'albums/index'
-  get 'albums/new'
-  get 'albums/show'
-  get 'album/index'
-  get 'album/new'
-  get 'album/show'
+  resources :albums
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
